07751001c63c2f592c9a6fbc494f72afabacfb1c
edilson gomes de lima <edskateicevida@gmail.com>
2023-07-29 20:21:40 -0300

    O ano de nascimento

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Meu primeiro codigo em csharp!");
+
+
+Console.Write("Digite o seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine("Digite o ano do seu Nascimaneto:");
+int year = int.Parse(Console.ReadLine());
+int age = 2022 - year;
+Console.WriteLine($"você tem {age} anos ");
